1c3c8d7d2d1c422fd4ff9204cd6f68f37fac68f0
unknown <xu_junkai@foxmail.com>
2019-02-27 11:43:27 +0800

    123

---
+++ roles/reportserver/files/init.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+old_password=$(cat /root/password.txt | awk -F ":" '{print $2}' )
+new_password=$(</dev/urandom tr -dc '12345!@#$%qwertQWERTasdfgASDFGzxcvbZXCVB' | head -c10)
+
+systemctl restart mysqld
+mysqladmin -uroot -p${old_password} password $new_password
+
+echo 'Datbases root Password:'$new_password  > /root/password.txt
+
+sed -i "s/123456/$new_password/" /data/wwwroot/reportserver/WEB-INF/classes/persistence.properties
+
+sed -i "s/\/root\/init.sh//" /etc/rc.local
+rm -rf /root/init.sh
+
