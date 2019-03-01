1e9e30341617936d209d1ff47d7ce3e765532a99
unknown <xu_junkai@foxmail.com>
2019-03-01 17:12:40 +0800

    123

--- roles/reportserver/files/init.sh
+++ roles/reportserver/files/init.sh
@@ -3,12 +3,15 @@
 old_password=$(cat /root/password.txt | awk -F ":" '{print $2}' )
 new_password=$(</dev/urandom tr -dc '12345!@#$%qwertQWERTasdfgASDFGzxcvbZXCVB' | head -c10)
 
-systemctl restart mysqld
+sleep 5
+systemctl restart mysql
 mysqladmin -uroot -p${old_password} password $new_password
 
 echo 'Datbases root Password:'$new_password  > /root/password.txt
 
-sed -i "s/123456/$new_password/" /data/wwwroot/reportserver/WEB-INF/classes/persistence.properties
+sed -i "s/$old_password/$new_password/" /data/wwwroot/reportserver/WEB-INF/classes/persistence.properties
+
+systemctl restart tomcat8
 
 sed -i "s/\/root\/init.sh//" /etc/rc.local
 rm -rf /root/init.sh
