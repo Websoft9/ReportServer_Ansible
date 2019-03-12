a64daf1a335cb247a68ffd234922eaa9ee772cd0
hliblack <hliblack@outlook.com>
2019-03-12 14:31:09 +0800

    merge

--- roles/reportserver/files/init.sh
+++ roles/reportserver/files/init.sh
@@ -3,7 +3,7 @@
 old_password=$(cat /root/password.txt | awk -F ":" '{print $2}' )
 new_password=$(</dev/urandom tr -dc '12345!@#$%qwertQWERTasdfgASDFGzxcvbZXCVB' | head -c10)
 
-systemctl restart mysqld
+systemctl restart mysql
 mysqladmin -uroot -p${old_password} password $new_password
 
 echo 'Datbases root Password:'$new_password  > /root/password.txt
