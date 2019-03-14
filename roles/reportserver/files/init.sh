158eb1b59a55ace850560192fc4481d53109bfee
unknown <xu_junkai@foxmail.com>
2019-03-14 08:56:03 +0800

    123

--- roles/reportserver/files/init.sh
+++ roles/reportserver/files/init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 old_password=$(cat /root/password.txt | awk -F ":" '{print $2}' )
-new_password=$(</dev/urandom tr -dc '12345!@#$%qwertQWERTasdfgASDFGzxcvbZXCVB' | head -c10)
+new_password=$(</dev/urandom tr -dc '12345qwertQWERTasdfgASDFGzxcvbZXCVB' | head -c10)
 
 sleep 5
 systemctl restart mysql
