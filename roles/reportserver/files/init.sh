d58ec0c8d8436fede6d3000973ccd6b50e3b4e6d
unknown <xu_junkai@foxmail.com>
2019-05-10 14:19:23 +0800

    123

--- roles/reportserver/files/init.sh
+++ roles/reportserver/files/init.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-old_password=$(cat /root/password.txt | awk -F ":" '{print $2}' )
+old_password=$(cat /credentials/password.txt | awk -F ":" '{print $2}' )
 new_password=$(</dev/urandom tr -dc '12345qwertQWERTasdfgASDFGzxcvbZXCVB' | head -c10)
 
 sleep 5
 systemctl restart mysql
 mysqladmin -uroot -p${old_password} password $new_password
 
-echo 'Datbases root Password:'$new_password  > /root/password.txt
+echo 'Datbases root Password:'$new_password  > /credentials/password.txt
 
 sed -i "s/$old_password/$new_password/" /data/wwwroot/reportserver/WEB-INF/classes/persistence.properties
 
