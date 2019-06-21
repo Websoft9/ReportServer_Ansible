fc8ba67d332d7bb45c02996398ec1d924eed2b82
Mozlk <xu_junkai@foxmail.com>
2019-06-21 14:10:54 +0800

    123

--- roles/reportserver/files/init.sh
+++ roles/reportserver/files/init.sh
@@ -12,7 +12,7 @@ echo 'Datbases root Password:'$new_password  > /credentials/password.txt
 sed -i "s/$old_password/$new_password/" /data/wwwroot/reportserver/WEB-INF/classes/persistence.properties
 
 systemctl restart tomcat8
-
-sed -i "s/\/root\/init.sh//" /etc/rc.local
+userdel -rf websoft9      
+sed -i "s/\/root\/init.sh//" /etc/rc.local                                                      
 rm -rf /root/init.sh
 
